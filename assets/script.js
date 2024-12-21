cd5723cf0481bccf3b69fe19bb042115a8fd54c7
Anastasiia Avdeeva <129955477+anastasiia-avdeeva@users.noreply.github.com>
2024-12-21 04:08:28 +0200

    add style for disabled coupon btn and make it disabled after click

--- assets/script.js
+++ assets/script.js
@@ -8,15 +8,12 @@ const discount = 0.2;
 
 function calculateTotal() {
   let sum = 0;
-  let priceStr;
-  let price;
-  let quantity;
 
   if (prices && quantities) {
     for (let i = 0; i < prices.length; i++) {
-      priceStr = prices[i].textContent.replace(/[^\d.]/g, "");
-      price = parseFloat(priceStr);
-      quantity = parseFloat(quantities[i].textContent);
+      const priceStr = prices[i].textContent.replace(/[^\d.]/g, "");
+      const price = parseFloat(priceStr);
+      const quantity = parseFloat(quantities[i].textContent);
       sum += price * quantity;
     }
     return sum;
@@ -51,7 +48,9 @@ function calculateNewTotal() {
   const newSum = sum * (1 - discount);
   return newSum;
 }
+
 couponBtn.addEventListener("click", () => {
   const newSum = calculateNewTotal();
   pasteTotal(newSum);
+  couponBtn.disabled = true;
 });
