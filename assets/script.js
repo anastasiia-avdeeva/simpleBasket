c1840aaf86be47e3e8cd27fe70f96177a9343a8b
Anastasiia Avdeeva <129955477+anastasiia-avdeeva@users.noreply.github.com>
2024-12-21 03:59:21 +0200

    add code for coupon usage and change event listeners invocation

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,10 @@
-const totalPar = document.querySelector(".total-paragraph");
 const total = document.getElementById("total-sum");
 const items = document.querySelector(".items");
-
+const couponBtn = document.getElementById("couponBtn");
 const prices = document.getElementsByClassName("item__price");
 const quantities = document.getElementsByClassName("quantity-val");
 const rubSymbol = "&#8381;";
+const discount = 0.2;
 
 function calculateTotal() {
   let sum = 0;
@@ -15,24 +15,43 @@ function calculateTotal() {
   if (prices && quantities) {
     for (let i = 0; i < prices.length; i++) {
       priceStr = prices[i].textContent.replace(/[^\d.]/g, "");
-      price = parseInt(priceStr);
-      quantity = parseInt(quantities[i].textContent);
+      price = parseFloat(priceStr);
+      quantity = parseFloat(quantities[i].textContent);
       sum += price * quantity;
     }
-
-    total.innerHTML = sum.toLocaleString("ru-RU") + rubSymbol;
+    return sum;
   }
 }
 
-window.addEventListener("load", calculateTotal);
+function pasteTotal(sum) {
+  total.innerHTML = sum.toLocaleString("ru-RU") + rubSymbol;
+}
+
+window.addEventListener("load", () => {
+  const sum = calculateTotal();
+  pasteTotal(sum);
+});
 
 function deleteItem(event) {
   const target = event.target;
   if (target.tagName === "BUTTON") {
     const itemToDel = target.closest(".item");
     itemToDel.remove();
-    calculateTotal();
   }
 }
 
-items.addEventListener("click", deleteItem);
+items.addEventListener("click", (e) => {
+  deleteItem(e);
+  const newTotal = calculateTotal();
+  pasteTotal(newTotal);
+});
+
+function calculateNewTotal() {
+  const sum = calculateTotal();
+  const newSum = sum * (1 - discount);
+  return newSum;
+}
+couponBtn.addEventListener("click", () => {
+  const newSum = calculateNewTotal();
+  pasteTotal(newSum);
+});
